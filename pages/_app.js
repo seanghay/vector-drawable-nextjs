4b10d00ebd29e2dbbb1480ad964e82882f15f3d2
Seanghay Yath <seanghay.dev@gmail.com>
2023-03-13 10:18:56 +0700

    feat: add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (<>
+    <Component {...pageProps} />
+    <Analytics />
+  </>)
 }
 
 export default MyApp
